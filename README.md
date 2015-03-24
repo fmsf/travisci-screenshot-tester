2239cca709fc00554b7c52042c7765e6cd537988
frferreira <frferreira@ebay.com>
2015-03-24 13:58:04 +0000

    added base test files to see if setup works on travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # travisci-screenshot-test
 Drop in module for travis CI to allow regression testing using screenshots.
+
+This is currently work in progress using the container-based infrastructure. Will add more stuff here once there I finish developing the base version
---
+++ test/screenshot-test.js
@@ -0,0 +1,74 @@
+var require = patchRequire(require),
+    fs = require( "fs" ),    
+    basePath = fs.absolute( fs.workingDirectory ),
+    testPath = basePath + "/test/html/",
+    Q = require( basePath + "/node_modules/q/q" ), // casper magix
+    phantomcss = require( basePath + "/node_modules/phantomcss/phantomcss" ); // casper magix
+// if anyone knows how to fix this for casperjs, feel free to remove the hard coded lib path
+
+
+function screenshotAndCompareFn( relativePath ) {
+
+    casper.test.begin( "Testing" + relativePath, function() {
+
+        console.log("Processing with path:", relativePath);
+
+        phantomcss.update( {
+            rebase: casper.cli.get( "rebase" ),
+            casper: casper,
+
+            libraryRoot: basePath + "/node_modules/phantomcss",
+            screenshotRoot: basePath + "/screenshots" ,
+            failedComparisonsRoot: basePath + "/screenshots/failed",
+
+            addLabelToFailedImage: false
+        });
+
+        casper.start( testPath + relativePath ); // points to the html file
+        casper.viewport( 1024, 768 );
+
+        casper.then( screenShotBody );    
+
+        casper.then( compareScreenShots );
+
+        casper.run( function() {
+            console.log(" Finished running tests for", relativePath);
+            casper.test.done();
+        });
+
+        function compareScreenShots() {
+            phantomcss.compareSession();
+        }
+
+        function screenShotBody() {
+            phantomcss.screenshot('body', relativePath ); // name of screenshot
+        }
+
+    });
+}
+
+
+function testRecursive( path ) {
+    var fileList = fs.list( testPath + path ),
+        targetFile,
+        targetPath,
+        i;    
+
+    for( i =0; i < fileList.length; i++ ) {
+        targetFile = fileList[i];
+
+        if ( targetFile != "." && targetFile != ".." ) {
+            targetPath = path + targetFile;
+
+            if( fs.isDirectory( testPath + targetPath ) ) {
+                testRecursive( targetPath + "/" );
+            } else {
+                console.log( "Queued:", targetPath);
+                screenshotAndCompareFn( targetPath );
+            }
+        }
+    }
+}
+
+testRecursive( "" );
+
